6ef255376f0191769bfbead83867a5409c3c9776
protldnetwork <protldnetwork@yandex.com>
2016-08-30 22:24:08 +0200

    Craft 1100b

---
+++ versions/1100b/craft/craft.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Craft 1100b
+    This file is a part of the Craft language. All rights reserved - (C) 2016-2016 ProTLD Network
+*/
+
+require('functions/say.craft.php');
+
+?>
---
+++ versions/1100b/craft/functions/say.craft.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Craft 1100b
+    This file is a part of the Craft language. All rights reserved - (C) 2016-2016 ProTLD Network
+*/
+
+function say($parameter)
+{
+	echo $parameter;
+}
+?>
